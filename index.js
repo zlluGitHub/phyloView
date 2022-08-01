28718653c6942f7d016ec9d79c2f72d218d5fe8d
zlluGitHub <18230086651@163.com>
2022-08-01 11:22:52 +0800

    # update import bug

---
+++ index.js
@@ -0,0 +1,6 @@
+import "./lib/phyloview.js";
+export const InitTreeStructure = window.phyloview.InitTreeStructure;
+export const InitRadialStructure = window.phyloview.InitRadialStructure;
+export const baseConfig = window.phyloview.baseConfig;
+export const InitTreeLayoutStructure = window.phyloview.InitTreeLayoutStructure;
+export default window.phyloview;
