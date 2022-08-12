b247f3abb77337a44bccaea66d5ab9900d11594f
NoteZ <18230086651@163.com>
2022-08-12 16:16:19 +0800

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import "./lib/phyloview.js";
-export const InitTreeStructure = window.phyloview.InitTreeStructure;
-export const InitRadialStructure = window.phyloview.InitRadialStructure;
-export const baseConfig = window.phyloview.baseConfig;
-export const InitTreeLayoutStructure = window.phyloview.InitTreeLayoutStructure;
-export default window.phyloview;
